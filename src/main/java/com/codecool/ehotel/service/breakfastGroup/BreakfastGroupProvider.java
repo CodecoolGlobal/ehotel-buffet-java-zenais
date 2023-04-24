79be41dd48f7233b200d07925534d375894613c6
zinaida <zinaida.j84@gmail.com>
2023-04-24 13:04:02 +0200

    Breakfast Group Provider class created

---
+++ src/main/java/com/codecool/ehotel/service/breakfastGroup/BreakfastGroupProvider.java
@@ -0,0 +1,13 @@
+package com.codecool.ehotel.service.breakfastGroup;
+
+import com.codecool.ehotel.model.Guest;
+
+import java.util.List;
+import java.util.Set;
+
+public class BreakfastGroupProvider {
+    public List<Set<Guest>> getBreakfastGroups(Set<Guest> allGuests, int numberOfGroups){
+        //TODO: Zinaida
+        return null;
+    }
+}
