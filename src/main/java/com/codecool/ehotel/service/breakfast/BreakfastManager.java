8a5443b6e95d025a3a84d02d417c19924b426048
zinaida <zinaida.j84@gmail.com>
2023-04-24 13:04:25 +0200

    breakfastManager class created

---
+++ src/main/java/com/codecool/ehotel/service/breakfast/BreakfastManager.java
@@ -0,0 +1,20 @@
+package com.codecool.ehotel.service.breakfast;
+
+import com.codecool.ehotel.model.Buffet;
+import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.model.GuestType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BreakfastManager {
+
+    public void serve(List<Set<Guest>> guestGroups){
+        //TODO:
+    }
+
+    public void getOptimalPortions(Buffet buffet, Map<GuestType, Integer > numberOfGuestsPerType, int cyclesLeft, int assumedCostOfUnhappyGuest){
+        //TODO:
+    }
+}
