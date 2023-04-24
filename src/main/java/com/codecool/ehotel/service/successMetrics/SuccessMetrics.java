4226e18ecb7ba2462f1ff402313afd5efcc16502
zinaida <zinaida.j84@gmail.com>
2023-04-24 13:03:29 +0200

    Success metrics class added

---
+++ src/main/java/com/codecool/ehotel/service/successMetrics/SuccessMetrics.java
@@ -0,0 +1,13 @@
+package com.codecool.ehotel.service.successMetrics;
+
+public class SuccessMetrics {
+    public int getNumberOfUnhappyGuests(){
+        //TODO: Zinaida
+        return -1;
+    }
+
+    public int getCostOfWastedFood(){
+        //TODO: Zinaida
+        return -1;
+    }
+}
