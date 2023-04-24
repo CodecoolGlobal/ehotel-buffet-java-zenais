bc28c80ff39f62d7a794986c7673583aaffc1818
zinaida <zinaida.j84@gmail.com>
2023-04-24 13:04:41 +0200

    Input class created

---
+++ src/main/java/com/codecool/ehotel/ui/Input.java
@@ -0,0 +1,12 @@
+package com.codecool.ehotel.ui;
+
+import java.util.Scanner;
+
+public class Input {
+    Scanner scanner;
+    public Input(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+
+}
