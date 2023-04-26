372982f29c38e89ce44f32c5489968058a9571da
Martin Egert <m.egert82@gmail.com>
2023-04-26 23:40:20 +0200

    finished unit tests for BuffetSerice

--- src/test/java/com/codecool/ehotel/service/buffet/BuffetServiceImplTest.java
+++ src/test/java/com/codecool/ehotel/service/buffet/BuffetServiceImplTest.java
@@ -5,18 +5,15 @@ import com.codecool.ehotel.model.Buffet;
 import com.codecool.ehotel.model.MealType;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuffetServiceImplTest {
     List<MealPortion> mealList = new ArrayList<>();
+    List<MealPortion> mealListForExpected = new ArrayList<>();
     Buffet buffet;
     BuffetService buffetService;
-
 
 
     private void BuffetServiceImplTest() {
@@ -26,27 +23,40 @@ class BuffetServiceImplTest {
         mealList.add(new MealPortion(MealType.MUFFIN, 3));
         mealList.add(new MealPortion(MealType.MILK, 1));
         mealList.add(new MealPortion(MealType.CEREAL, 3));
+        mealListForExpected.add(new MealPortion(MealType.SCRAMBLED_EGGS, 1));
+        mealListForExpected.add(new MealPortion(MealType.SUNNY_SIDE_UP, 3));
+        mealListForExpected.add(new MealPortion(MealType.MASHED_POTATO, 1));
+        mealListForExpected.add(new MealPortion(MealType.MUFFIN, 3));
+        mealListForExpected.add(new MealPortion(MealType.MILK, 1));
+        mealListForExpected.add(new MealPortion(MealType.CEREAL, 3));
         Buffet buffet = new Buffet(mealList);
         this.buffet = buffet;
         this.buffetService = new BuffetServiceImpl(buffet);
     }
 
-   /* @Test
+    @Test
     void refill() {
+        BuffetServiceImplTest();
+        List<MealPortion> expected = mealListForExpected;
+        expected.add(new MealPortion(MealType.BUN, 4));
+        expected.add(new MealPortion(MealType.BUN, 4));
+        expected.add(new MealPortion(MealType.BUN, 4));
+        expected.add(new MealPortion(MealType.BUN, 4));
         Map<MealType, Integer> mealMap = new HashMap<>();
         mealMap.put(MealType.BUN, 4);
         int cycle = 4;
         buffetService.refill(mealMap, cycle);
+        List<MealPortion> result = buffet.mealsOnDisplay();
+        System.out.println(expected);
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
 
-        //buffetService.refill();
-
-    }*/
-
-    /*@Test
+    @Test
     void consumeFreshest() {
         BuffetServiceImplTest();
         assertTrue(buffetService.consumeFreshest(MealType.SCRAMBLED_EGGS));
-    }*/
+    }
 
     @Test
     void collectWaste() {
