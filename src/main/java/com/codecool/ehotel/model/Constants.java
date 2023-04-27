156a32f525dd9ed01e959e2f572efb5649e9c1b0
zinaida <zinaida.j84@gmail.com>
2023-04-27 18:44:29 +0200

    MAX_STAY added to constants

--- src/main/java/com/codecool/ehotel/model/Constants.java
+++ src/main/java/com/codecool/ehotel/model/Constants.java
@@ -7,4 +7,5 @@ public interface Constants {
     int ALL_DINNER_GUESTS = 100;
     LocalDate SEASON_START = LocalDate.parse("2022-01-01");
     LocalDate SEASON_END = LocalDate.parse("2022-02-01");
+    int MAX_STAY_DAYS = 7;
 }
