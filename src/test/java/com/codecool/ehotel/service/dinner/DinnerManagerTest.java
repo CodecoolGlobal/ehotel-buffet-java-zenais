cf76ba5df17798210992745e2388d4b294566719
zinaida <zinaida.j84@gmail.com>
2023-04-27 13:30:08 +0200

    KitchenService Object argument updated

--- src/test/java/com/codecool/ehotel/service/dinner/DinnerManagerTest.java
+++ src/test/java/com/codecool/ehotel/service/dinner/DinnerManagerTest.java
@@ -4,14 +4,14 @@ import com.codecool.ehotel.model.DinnerGuestType;
 import com.codecool.ehotel.model.Guest;
 import com.codecool.ehotel.service.breakfastGroup.BreakfastGroupProvider;
 import com.codecool.ehotel.service.date.DateService;
+import com.codecool.ehotel.service.dinner.kitchen.DinnerService;
 import com.codecool.ehotel.service.dinner.kitchen.KitchenService;
-import com.codecool.ehotel.service.dinner.kitchen.Storage;
+import com.codecool.ehotel.service.dinner.kitchen.StorageService;
 import com.codecool.ehotel.service.guest.GuestServiceImpl;
 import com.codecool.ehotel.service.successMetrics.SuccessMetrics;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,7 +24,7 @@ class DinnerManagerTest {
     GuestServiceImpl<DinnerGuestType> dinnerGuestService = new GuestServiceImpl<DinnerGuestType>(dateService, DinnerGuestType.values());
     List<Guest> allDinnerGuests = dinnerGuestService.generateAllGuests(seasonStart, seasonEnd, numberOfGuests, 1);
     DinnerService dinnerService = new DinnerService();
-    KitchenService kitchenService = new KitchenService(new Storage(new LinkedList<>()));
+    KitchenService kitchenService = new KitchenService(new StorageService(new LinkedList<>()));
     SuccessMetrics successMetrics = new SuccessMetrics(0,0);
     DinnerManager dinnerManager = new DinnerManager(allDinnerGuests, seasonStart, seasonEnd, breakfastGroupProvider, dinnerGuestService, dinnerService, kitchenService, successMetrics);
 
