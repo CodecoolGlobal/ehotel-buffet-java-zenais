4d0c6613ff7ca2010dc07c8ea5acf56c23234391
zinaida <zinaida.j84@gmail.com>
2023-04-27 13:32:18 +0200

    getOrders renamed

--- src/test/java/com/codecool/ehotel/service/dinner/DinnerServiceTest.java
+++ src/test/java/com/codecool/ehotel/service/dinner/DinnerServiceTest.java
@@ -1,19 +1,15 @@
 package com.codecool.ehotel.service.dinner;
 
-import com.codecool.ehotel.model.Dinner;
 import com.codecool.ehotel.model.DinnerGuestType;
 import com.codecool.ehotel.model.Guest;
 import com.codecool.ehotel.service.date.DateService;
+import com.codecool.ehotel.service.dinner.kitchen.DinnerService;
 import com.codecool.ehotel.service.guest.GuestServiceImpl;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DinnerServiceTest {
 
@@ -23,7 +19,7 @@ class DinnerServiceTest {
     DinnerService dinnerService = new DinnerService();
     @Test
     void getOrdersTest() {
-       System.out.println( dinnerService.getOrders(new HashSet<>(allDinnerGuests)));
+       System.out.println( dinnerService.createOrdersFromPreferences(new HashSet<>(allDinnerGuests)));
     }
 
     @Test
