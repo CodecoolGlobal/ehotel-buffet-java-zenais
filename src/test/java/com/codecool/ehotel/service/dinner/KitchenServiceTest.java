469650c4884173fe589fb2f7828548182a78233a
zinaida <zinaida.j84@gmail.com>
2023-04-27 13:34:17 +0200

    Storage renamed to StorageService

--- src/test/java/com/codecool/ehotel/service/dinner/KitchenServiceTest.java
+++ src/test/java/com/codecool/ehotel/service/dinner/KitchenServiceTest.java
@@ -4,13 +4,11 @@ import com.codecool.ehotel.model.DinnerGuestType;
 import com.codecool.ehotel.model.Guest;
 import com.codecool.ehotel.service.date.DateService;
 import com.codecool.ehotel.service.dinner.kitchen.KitchenService;
-import com.codecool.ehotel.service.dinner.kitchen.Storage;
+import com.codecool.ehotel.service.dinner.kitchen.StorageService;
 import com.codecool.ehotel.service.guest.GuestServiceImpl;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,7 +16,7 @@ class KitchenServiceTest {
     DateService dateService = new DateService();
     GuestServiceImpl<DinnerGuestType> dinnerGuestService = new GuestServiceImpl<DinnerGuestType>(dateService, DinnerGuestType.values());
     List<Guest> allDinnerGuests = dinnerGuestService.generateAllGuests(LocalDate.now(), LocalDate.now(), 1, 1);
-    KitchenService kitchenService = new KitchenService(new Storage(new LinkedList<>()));
+    KitchenService kitchenService = new KitchenService(new StorageService(new LinkedList<>()));
     @Test
     void serveOrders() {
     }
