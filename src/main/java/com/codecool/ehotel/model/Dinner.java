3a1693eaa6139613237f16437be702d59ae44ca5
zinaida <zinaida.j84@gmail.com>
2023-04-27 18:50:31 +0200

    Dinner price updated

--- src/main/java/com/codecool/ehotel/model/Dinner.java
+++ src/main/java/com/codecool/ehotel/model/Dinner.java
@@ -6,14 +6,14 @@ import static com.codecool.ehotel.model.Ingredient.*;
 
 public enum Dinner {
 
-    Simple_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, MINCED_MEAT, ONION, KETCHUP), 0.17, 7),
-    Avocado_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, MINCED_MEAT, AVOCADO), 0.17, 9),
-    Veggie_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, AVOCADO, SOY_STEAK, ONION, KETCHUP), 0.06, 9),
-    Veggie_Soup(Set.of(COOKED_VEGETABLE), 0.1, 5),
-    Minestrone_Soup(Set.of(COOKED_VEGETABLE, SLICED_TOMATO, NOODLES), 0.06, 5),
-    Spiderman_Spagetti(Set.of(SPAGHETTI, TOMATO_SOUCE, MINCED_MEAT), 0.13, 7),
-    Pommes(Set.of(POTATO, KETCHUP), 0.13, 3),
-    Big_salat(Set.of(LETTUCE, ONION, AVOCADO, EGG, MAYONNAISE), 0.06, 8),
+    Simple_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, MINCED_MEAT, ONION, KETCHUP), 0.17, 42),
+    Avocado_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, MINCED_MEAT, AVOCADO), 0.17, 41),
+    Veggie_burger(Set.of(BURGER_BUNS, LETTUCE, SLICED_TOMATO, AVOCADO, SOY_STEAK, ONION, KETCHUP), 0.06, 42),
+    Veggie_Soup(Set.of(COOKED_VEGETABLE), 0.1, 8),
+    Minestrone_Soup(Set.of(COOKED_VEGETABLE, SLICED_TOMATO, NOODLES), 0.06, 15),
+    Spiderman_Spagetti(Set.of(SPAGHETTI, TOMATO_SOUCE, MINCED_MEAT), 0.13, 18),
+    Pommes(Set.of(POTATO, KETCHUP), 0.13, 2),
+    Big_salat(Set.of(LETTUCE, ONION, AVOCADO, EGG, MAYONNAISE), 0.06, 41),
     NO_DINNER(Set.of(), 0, 0);
 
     private Set<Ingredient> ingredients;
