943f7b22ad284b501b3191d1756ec6392c653674
zinaida <zinaida.j84@gmail.com>
2023-04-27 11:46:40 +0200

    KitchenServiceTest created

---
+++ src/test/java/com/codecool/ehotel/service/dinner/KitchenServiceTest.java
@@ -0,0 +1,34 @@
+package com.codecool.ehotel.service.dinner;
+
+import com.codecool.ehotel.model.DinnerGuestType;
+import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.service.date.DateService;
+import com.codecool.ehotel.service.dinner.kitchen.KitchenService;
+import com.codecool.ehotel.service.dinner.kitchen.Storage;
+import com.codecool.ehotel.service.guest.GuestServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+class KitchenServiceTest {
+    DateService dateService = new DateService();
+    GuestServiceImpl<DinnerGuestType> dinnerGuestService = new GuestServiceImpl<DinnerGuestType>(dateService, DinnerGuestType.values());
+    List<Guest> allDinnerGuests = dinnerGuestService.generateAllGuests(LocalDate.now(), LocalDate.now(), 1, 1);
+    KitchenService kitchenService = new KitchenService(new Storage(new LinkedList<>()));
+    @Test
+    void serveOrders() {
+    }
+
+    @Test
+    void supplyKitchenTest() {
+        //kitchenService.supplyKitchen(new HashSet<>(allDinnerGuests));
+    }
+
+    @Test
+    void cleanKitchen() {
+    }
+}
