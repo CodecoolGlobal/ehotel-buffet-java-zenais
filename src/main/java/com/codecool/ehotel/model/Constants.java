3155119f685a77c2ebc6d9e49667bb13040a96d5
zinaida <zinaida.j84@gmail.com>
2023-04-27 15:10:25 +0200

    Constants Interface created

---
+++ src/main/java/com/codecool/ehotel/model/Constants.java
@@ -0,0 +1,10 @@
+package com.codecool.ehotel.model;
+
+import java.time.LocalDate;
+
+public interface Constants {
+    int ALL_BREAKFAST_GUESTS = 150;
+    int ALL_DINNER_GUESTS = 100;
+    LocalDate SEASON_START = LocalDate.parse("2022-01-01");
+    LocalDate SEASON_END = LocalDate.parse("2022-02-01");
+}
