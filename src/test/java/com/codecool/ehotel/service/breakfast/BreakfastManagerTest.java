f91c53bc49640d1a3df83aed86d70dda7c428380
Martin Egert <m.egert82@gmail.com>
2023-04-27 13:21:32 +0200

    added presentation

---
+++ src/test/java/com/codecool/ehotel/service/breakfast/BreakfastManagerTest.java
@@ -0,0 +1,12 @@
+package com.codecool.ehotel.service.breakfast;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreakfastManagerTest {
+
+    @Test
+    void getOptimalPortions() {
+    }
+}
